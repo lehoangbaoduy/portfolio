74bfe6ad63c49c1fb0bfe34c92683f0b7eee512c
Hoang Bao Duy Le <kmktag904@gmail.com>
2020-11-24 20:15:17 -0500

    first commit

---
+++ js/scrolling.js
@@ -0,0 +1,31 @@
+function goTop() {
+    window.scrollTo(0, 0);
+}
+function aboutSection() {
+    let about = document.querySelector("#about-main");
+    window.scrollTo(0, about.offsetTop);
+}
+function experienceSection() {
+    let experience = document.querySelector("#experience-main");
+    window.scrollTo(0, experience.offsetTop);
+}
+function skillSection() {
+    let skill = document.querySelector("#skill-main");
+    window.scrollTo(0, skill.offsetTop);
+}
+function educationSection() {
+    let education = document.querySelector("#education-main");
+    window.scrollTo(0, education.offsetTop);
+}
+function projectSection() {
+    let project = document.querySelector("#project-main");
+    window.scrollTo(0, project.offsetTop);
+}
+function awardSection() {
+    let award = document.querySelector("#award-main");
+    window.scrollTo(0, award.offsetTop);
+}
+function contactSection() {
+    let contact = document.querySelector("#contact-main");
+    window.scrollTo(0, contact.offsetTop);
+}
