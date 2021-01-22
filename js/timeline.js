e16809e8a75771f172b446ab83809321ccf11320
Hoang Bao Duy Le <69600640+lehoangbaoduy@users.noreply.github.com>
2021-01-22 09:45:49 -0500

    Add sixth step

--- js/timeline.js
+++ js/timeline.js
@@ -5,12 +5,14 @@ function showst1() {
     $("#third-inner").css('color', 'white');
     $("#fourth-inner").css('color', 'white');
     $("#fifth-inner").css('color', 'white');
+    $("#sixth-inner").css('color', 'white');
 
     $(".st1").addClass("active");
     $(".st2").removeClass("active");
     $(".st3").removeClass("active");
     $(".st4").removeClass("active");
     $(".st5").removeClass("active");
+    $(".st6").removeClass("active");
 }
 function showst2() {
     $(".second").prop("checked", true);
@@ -19,12 +21,14 @@ function showst2() {
     $("#third-inner").css('color', 'white');
     $("#fourth-inner").css('color', 'white');
     $("#fifth-inner").css('color', 'white');
+    $("#sixth-inner").css('color', 'white');
 
     $(".st2").addClass("active");
     $(".st1").addClass("active");
     $(".st3").removeClass("active");
     $(".st4").removeClass("active");
     $(".st5").removeClass("active");
+    $(".st6").removeClass("active");
 }
 function showst3() {
     $(".third").prop("checked", true);
@@ -33,12 +37,14 @@ function showst3() {
     $("#first-inner").css('color', '#08fdd8');
     $("#fourth-inner").css('color', 'white');
     $("#fifth-inner").css('color', 'white');
+    $("#sixth-inner").css('color', 'white');
 
     $(".st3").addClass("active");
     $(".st1").addClass("active");
     $(".st2").addClass("active");
     $(".st4").removeClass("active");
     $(".st5").removeClass("active");
+    $(".st6").removeClass("active");
 }
 function showst4() {
     $(".fourth").prop("checked", true);
@@ -47,12 +53,14 @@ function showst4() {
     $("#second-inner").css('color', '#08fdd8');
     $("#first-inner").css('color', '#08fdd8');
     $("#fifth-inner").css('color', 'white');
+    $("#sixth-inner").css('color', 'white');
 
     $(".st4").addClass("active");
     $(".st1").addClass("active");
     $(".st2").addClass("active");
     $(".st3").addClass("active");
     $(".st5").removeClass("active");
+    $(".st6").removeClass("active");
 }
 function showst5() {
     $(".fifth").prop("checked", true);
@@ -61,10 +69,28 @@ function showst5() {
     $("#third-inner").css('color', '#08fdd8');
     $("#second-inner").css('color', '#08fdd8');
     $("#first-inner").css('color', '#08fdd8');
+    $("#sixth-inner").css('color', 'white');
 
     $(".st5").addClass("active");
     $(".st1").addClass("active");
     $(".st2").addClass("active");
     $(".st3").addClass("active");
     $(".st4").addClass("active");
+    $(".st6").removeClass("active");
+}
+function showst6() {
+    $(".sixth").prop("checked", true);
+    $("#fifth-inner").css('color', '#08fdd8');
+    $("#fourth-inner").css('color', '#08fdd8');
+    $("#third-inner").css('color', '#08fdd8');
+    $("#second-inner").css('color', '#08fdd8');
+    $("#first-inner").css('color', '#08fdd8');
+    $("#sixth-inner").css('color', '#08fdd8');
+
+    $(".st5").addClass("active");
+    $(".st1").addClass("active");
+    $(".st2").addClass("active");
+    $(".st3").addClass("active");
+    $(".st4").addClass("active");
+    $(".st6").addClass("active");
 }
